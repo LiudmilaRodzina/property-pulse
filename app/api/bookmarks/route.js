d5451ec099248a777919dad9b2ce4c1a2dcd833c
Liudmila Rodzina <liudmilarodzina@gmail.com>
2025-01-30 18:23:20 +0300

    feat: display bookmarked properties on saved properties page

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -1,8 +1,30 @@
 import connectDB from '@/config/database';
 import { getSessionUser } from '@/utils/getSessionUser';
 import User from '@/models/User';
+import Property from '@/models/Property';
 
 export const dynamic = 'force-dynamic';
+
+// GET /api/bookmarks
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+    if (!sessionUser || !sessionUser.user.id)
+      return new Request('User ID is required', { status: 401 });
+    const { userId } = sessionUser;
+
+    const user = await User.findOne({ _id: userId });
+
+    const bookmarks = await Property.find({ _id: { $in: user.bookmarks } });
+
+    return new Response(JSON.stringify(bookmarks), { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return new Response('Something went wrong', { status: 500 });
+  }
+};
 
 export const POST = async (request) => {
   try {
