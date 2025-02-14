13caf596890d5d60f4e2f42871f6e4d71d882538
Liudmila Rodzina <liudmilarodzina@gmail.com>
2025-02-14 12:56:28 +0300

    feat: display featured properties on home page

--- utils/requests.js
+++ utils/requests.js
@@ -1,12 +1,13 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
-// Fetch all properties
-const fetchProperties = async () => {
+const fetchProperties = async ({ showFeatured = false } = {}) => {
   try {
-    // handle the case where the domain is not available yet
     if (!apiDomain) return [];
 
-    const res = await fetch(`${apiDomain}/properties`, { cache: 'no-store' });
+    const res = await fetch(
+      `${apiDomain}/properties${showFeatured ? '/featured' : ''}`,
+      { cache: 'no-store' }
+    );
 
     if (!res.ok) throw new Error('Failed to fetch data');
 
@@ -18,7 +19,6 @@ const fetchProperties = async () => {
   }
 };
 
-// Fetch a single property
 const fetchProperty = async (id) => {
   try {
     if (!apiDomain) return null;
