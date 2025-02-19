0219fd4917f07cf4751829a1f8ad3705ca3cfcd5
Liudmila Rodzina <liudmilarodzina@gmail.com>
2025-02-19 13:39:47 +0300

    feat: create featured properties page

---
+++ app/properties/featured/page.jsx
@@ -0,0 +1,64 @@
+'use client';
+import { useState, useEffect } from 'react';
+import PropertyCard from '@/components/PropertyCard';
+import Spinner from '@/components/Spinner';
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
+
+const FeaturedPropertiesPage = () => {
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState([]);
+
+  useEffect(() => {
+    const fetchFeaturedProperties = async () => {
+      try {
+        const res = await fetch('/api/properties/featured');
+
+        if (res.status === 200) {
+          const data = await res.json();
+          setProperties(data);
+        }
+      } catch (error) {
+        console.error(error);
+        toast.error('Failed to fetch saved properties');
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchFeaturedProperties();
+  }, []);
+
+  return loading ? (
+    <Spinner loading={loading} />
+  ) : (
+    <>
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" />
+            Back to Home
+          </Link>
+        </div>
+      </section>
+      <section className="px-4 py-6">
+        <h2 className="text-center text-2xl mb-4">Featured Properties</h2>
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          {properties.length === 0 ? (
+            <div>No featured properties</div>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default FeaturedPropertiesPage;
--- components/FeaturedProperties.jsx
+++ components/FeaturedProperties.jsx
@@ -1,23 +1,35 @@
 import { fetchProperties } from '@/utils/requests';
 import FeaturedPropertyCard from './FeaturedPropertyCard';
+import Link from 'next/link';
 
 const FeaturedProperties = async () => {
-  const properties = await fetchProperties({ showFeatured: true });
+  const data = await fetchProperties({ showFeatured: true });
+  const properties = data.sort(() => Math.random() - Math.random()).slice(0, 2);
 
   return (
     properties.length > 0 && (
-      <section className="bg-blue-50 px-4 pt-6 pb-10">
-        <div className="container-xl lg:container m-auto">
-          <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
-            Featured Properties
-          </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            {properties.map((property) => (
-              <FeaturedPropertyCard key={property._id} property={property} />
-            ))}
+      <>
+        <section className="bg-blue-50 px-4 pt-6 pb-10">
+          <div className="container-xl lg:container m-auto">
+            <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
+              Featured Properties
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              {properties.map((property) => (
+                <FeaturedPropertyCard key={property._id} property={property} />
+              ))}
+            </div>
           </div>
-        </div>
-      </section>
+          <div className="m-auto max-w-lg mt-10 mb-2 px-6">
+            <Link
+              href="/properties/featured"
+              className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+            >
+              View All Featured Properties
+            </Link>
+          </div>
+        </section>
+      </>
     )
   );
 };
